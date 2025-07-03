98b819030b671a60ea1b31c5092fbe4f64357fd2
Amirhosein <ah.rezaie07@gmail.com>
2025-07-02 23:10:17 -0700

    add the app to the install apps

--- EnglishClass/settings.py
+++ EnglishClass/settings.py
@@ -37,6 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'rest_framework',
+    'people',
+    'education',
+    'core',
+    'drf_spectacular',
 ]
 
 MIDDLEWARE = [

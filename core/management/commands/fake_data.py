e1c6925cde31a790ff81dfe841614f3607389875
Amirhosein <ah.rezaie07@gmail.com>
2025-07-04 01:06:18 -0700

    test and debug the commands

--- core/management/commands/fake_data.py
+++ core/management/commands/fake_data.py
@@ -6,10 +6,8 @@ from core import models as CoreModels
 from people import models as PeopleModels
 from education import models as EducationModels
 
-class commend(BaseCommand):
+class Command(BaseCommand):
     help = "Creating the fake data in db ... "
 
     def handle(self, *args, **options):
-        pass
-
-        return super().handle(*args, **options)
+        print("done")

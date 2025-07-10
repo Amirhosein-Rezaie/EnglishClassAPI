21585043b923f5944826b10031899cb706b134ad
Amirhosein <ah.rezaie07@gmail.com>
2025-07-09 20:41:12 -0700

    hash the password while create and update user

--- core/serializers.py
+++ core/serializers.py
@@ -1,5 +1,6 @@
 from rest_framework.serializers import ModelSerializer, CharField
 from . import models
+from django.contrib.auth.hashers import make_password
 
 
 # users profiles
@@ -12,11 +13,27 @@ class UserProfileSerializer(ModelSerializer):
 # users
 class UserSerializer(ModelSerializer):
     password = CharField(write_only=True)
-    profiles = UserProfileSerializer(source='user_profiles', many=True)
+    profiles = UserProfileSerializer(
+        source='user_profiles', many=True, read_only=True)
 
     class Meta:
         model = models.Users
         fields = '__all__'
+
+    def create(self, validated_data):
+        validated_data['password'] = make_password(validated_data['password'])
+        return super().create(validated_data)
+
+    def update(self, instance, validated_data):
+        password = validated_data.pop('password', None)
+        if password:
+            instance.password = make_password(password)
+
+        for attr, value in validated_data.items():
+            setattr(instance, attr, value)
+
+        instance.save()
+        return instance
 
 
 # levels

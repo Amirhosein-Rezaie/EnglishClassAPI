9c00c94fc37ad5ff51e4551a9a7aaa16da5320c2
amirhosein <amirhossein48x90@gmail.com>
2025-07-14 13:56:41 -0700

    search the fields startwith but id

--- EnglishClass/helper.py
+++ EnglishClass/helper.py
@@ -2,8 +2,7 @@ from rest_framework.request import Request
 from django.core.exceptions import FieldError
 from rest_framework.response import Response
 from rest_framework import status
-from django.db.models import Q
-from django.db.models import Model
+from django.db.models import Q, Model
 from rest_framework.serializers import ModelSerializer
 
 
@@ -12,13 +11,17 @@ def dynamic_search(request: Request, model: Model, serializer: ModelSerializer):
     a function that you can have dynamic search in every ever
     """
     query_params = request.query_params
-    try:
-        query_search = Q()
-        for key, value in query_params.items():
-            query_search &= Q(**{f"{key}__icontains": value})
-        founds = model.objects.filter(query_search)
-        return Response(serializer(founds, many=True).data, status=status.HTTP_200_OK)
-    except FieldError:
-        return Response({
-            "detials": f"پارامتر های ارسالی معتبر نیستند"
-        }, status=status.HTTP_400_BAD_REQUEST)
+    if query_params:
+        try:
+            query_search = Q()
+            for key, value in query_params.items():
+                field = key
+                if key not in ['id']:
+                    field += "__istartswith"
+                query_search &= Q(**{f"{field}": value})
+            founds = model.objects.filter(query_search)
+            return Response(serializer(founds, many=True).data, status=status.HTTP_200_OK)
+        except FieldError:
+            return Response({
+                "details": f"پارامتر های ارسالی معتبر نیستند"
+            }, status=status.HTTP_400_BAD_REQUEST)

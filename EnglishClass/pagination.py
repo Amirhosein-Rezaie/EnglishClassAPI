3724d3f2e4c6cea6070fad83a0baa2ad4ba58983
Amirhosein <ah.rezaie07@gmail.com>
2025-07-14 19:46:31 -0700

    set description in custom paginations

--- EnglishClass/pagination.py
+++ EnglishClass/pagination.py
@@ -3,6 +3,9 @@ from rest_framework.request import Request
 
 
 class DynamicPagination(PageNumberPagination):
+    """
+    a class that you can have custom pagination by limit in queryparams
+    """
     page_size = 10
 
     def get_page_size(self, request: Request):
